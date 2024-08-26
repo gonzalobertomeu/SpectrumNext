52ac641fb1ba275c153ca5a71e39a62bf2f52bc3
Gonzalo Bertomeu <gonzalobertomeu@pop-os.localdomain>
2024-08-25 21:30:36 -0300

    Git rm main/

--- main/shared_interfaces/IWinAction.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IWinAction = void 0;
-var IWinAction;
-(function (IWinAction) {
-    IWinAction["MINIMIZE"] = "minimize";
-    IWinAction["MAXIMIZE"] = "maximize";
-    IWinAction["CLOSE"] = "close";
-})(IWinAction = exports.IWinAction || (exports.IWinAction = {}));
--- main/shared_interfaces/WinActions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WinAction = void 0;
-var WinAction;
-(function (WinAction) {
-    WinAction["MINIMIZE"] = "minimize";
-    WinAction["MAXIMIZE"] = "maximize";
-    WinAction["CLOSE"] = "close";
-})(WinAction = exports.WinAction || (exports.WinAction = {}));
